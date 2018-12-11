c3f788e78de3288af5e9e34cf9d5982f681966a9
IshiiIsaku <45581362+IshiiIsaku@users.noreply.github.com>
2018-12-11 21:13:00 +0900

    Update PTra09_01.java

--- Practice/Practice/src/practice09/PTra09_01.java
+++ Practice/Practice/src/practice09/PTra09_01.java
@@ -1,4 +1,7 @@
 package practice09;
+
+import practice09.util.Util;
+
 /*
  * PTra09_01.java
  *   作成	LIKEIT	2017
@@ -10,6 +13,12 @@ public class PTra09_01 {
 	public static void main(String[] args) {
 		String str = "この文字列をゆっくり表示したい";
 
+		 Util.dispMessageSlowly(str, 500);
+
+
+	}
+
+
 		// ★ 変数strを、Util内のメソッドを用いて0.5秒間隔で表示させなさい
 		// ※ Utilはインポートしても良いし、完全限定名で記述しても良いです
 
@@ -18,27 +27,19 @@ public class PTra09_01 {
 		 * @param message	表示する文字列
 		 * @param millisecond	間隔をあけるミリ秒（1000で1秒）
 		 */
-		public static void dispMessageSlowly(String message, long millisecond) {
-			char[] array = message.toCharArray();
-			for (int i = 0; i < message.length(); i++) {
-				System.out.print(array[i]);
-				try {
-					Thread.sleep(millisecond);
-				} catch (InterruptedException e) {
-					/* エラーが発生した場合はここにくる */
-				}
-			}
-		}
-
-
-
-
-
-
-	}
-
-
 
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
+

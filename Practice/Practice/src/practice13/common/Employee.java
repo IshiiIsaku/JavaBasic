77045dfb63c0833f10bc886bb37978bbe32ac975
IshiiIsaku <45581362+IshiiIsaku@users.noreply.github.com>
2018-12-18 15:15:48 +0900

    Create Employee.java

---
+++ Practice/Practice/src/practice13/common/Employee.java
@@ -0,0 +1,23 @@
+package practice13.common;
+
+public class Employee extends Person{
+
+	
+	String[] 	departmentNm;
+	String[] departmentCnt;
+	public String[] getDepartmentNm() {
+		return departmentNm;
+	}
+	public void setDepartmentNm(String[] quaterdata) {
+		this.departmentNm = quaterdata;
+	}
+	public String[] getDepartmentCnt() {
+		return departmentCnt;
+	}
+	public void setDepartmentCnt(String[] quaterdata) {
+		this.departmentCnt = quaterdata;
+	}
+	
+	
+	
+}
